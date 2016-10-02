2a23b1a1193e9cbca86ac54b77afd31bc4ee175b
Lokesh <lrbeeram@gmail.com>
2016-10-03 01:59:17 +0530

    submission

--- js/index.js
+++ js/index.js
@@ -49,26 +49,20 @@ var Location = function(data) {
     this.marker.addListener('click', function() {
 
         populateInfoWindow(this, largeInfowindow);
-        toggleBounce(this)
+        toggleBounce(this);
     });
 
 
 
-}
+};
 
 var foursquare_CLIENT_ID = 'E5RPW3KFBNWGODCETUOV2KXBIKDXPGYSE4LE0OKHGSFUD4KO';
 var foursquare_CLIENT_SECRET = 'PCKC1QJNNVO55AWV2J5ZQAXWJ4XGAT0X2NO0BXNTZNC3EPGH';
 var foursquareUrl;
 var streetViewUrl;
 
-var stringStartsWith = function(string, startsWith) {
-    string = string || "";
-    if (startsWith.length > string.length)
-        return false;
-    return string.substring(0, startsWith.length) === startsWith;
-};
 
-var viewModel = function() {
+var ViewModel = function() {
     var self = this;
     self.locationList = ko.observableArray([]);
     self.filter = ko.observable("");
@@ -79,22 +73,26 @@ var viewModel = function() {
             marker.setAnimation(null);
         } else {
             marker.setAnimation(google.maps.Animation.BOUNCE);
+            setTimeout(function() { marker.setAnimation(null); }, 700);
+
         }
-    }
+    };
     self.populateInfoWindow = function(marker, infowindow) {
         // Check to make sure the infowindow is not already opened on this marker.
+        var link;
+        var rating;
         if (infowindow.marker != marker) {
-            infowindow.marker = marker;
-            infowindow.setContent('<div id = "marker-title"><h2>' + marker.title + '</h2></div>');
-            infowindow.open(map, marker);
-            // Make sure the marker property is cleared if the infowindow is closed.
-            infowindow.addListener('closeclick', function() {
-                infowindow.setMarker(null);
-            });
 
+            infowindow.marker = marker;
+            infowindow.open(map, marker);
+            setTimeout(function () {  }, 5000);
+            // Make sure the marker property is cleared if the infowindow is closed.
+            infowindow.addListener('closeclick,', function() {
+            infowindow.setMarker(null);
+               });
+            
 
             streetViewUrl = 'https://maps.googleapis.com/maps/api/streetview?size=200x100&location=' + marker.position.lat() + ',' + marker.position.lng() + '&fov=90&heading=235&pitch=10';
-            $('#marker-title').append('<img src =' + streetViewUrl + '>');
             foursquareUrl = 'https://api.foursquare.com/v2/venues/explore?client_id=' +
                 foursquare_CLIENT_ID + '&client_secret=' + foursquare_CLIENT_SECRET +
                 '&m=foursquare&v=20140806&ll=' + marker.position.lat() + ',' +
@@ -108,10 +106,19 @@ var viewModel = function() {
                     var rating = data.response.groups[0].items[0].venue.rating;
                     if (!link && !rating) {
                         // this means nothing about the address
-                        $('#marker-title').append('<h3>This place has not been on Foursquare.</h3>');
+                        infowindow.setContent('<div id = "marker-title"><h2>' + marker.title + '</h2></div><br>'+
+                            '<h3>This place has not been on Foursquare.</h3>');
+
+                      
                     } else {
                         // append the content after #marker-title in infowindow
-                        $('#marker-title').append('<h3>Foursquare Rating: <span class="rating">' +
+                        if (rating === undefined) {
+                            rating = "Not available";
+
+                        }
+                        infowindow.setContent('<div id = "marker-title"><h2>' + marker.title + '</h2></div><br>'
+                            +'<img src =' + streetViewUrl + '><br>'
+                             +'<h3>Foursquare Rating: <span class="rating">' +
                             rating + '</span></h3>' + '<a class="fsqure-link" href="' +
                             link + '" target="new">Foursquare Link</a>');
                     }
@@ -120,11 +127,11 @@ var viewModel = function() {
                     alert('Foursquare data failed to load.');
                 });
         }
-    }
+    };
 
     self.showInfo = function(location) {
-        google.maps.event.trigger(location.marker, 'click')
-    }
+        google.maps.event.trigger(location.marker, 'click');
+    };
 
     initialLocations.forEach(function(item) {
         self.locationList.push(new Location(item));
@@ -139,27 +146,27 @@ var viewModel = function() {
             return this.locationList();
         } else {
             return ko.utils.arrayFilter(this.locationList(), function(loc) {
-                if (stringStartsWith(loc.title().toLowerCase(), self.search)) {
+                if (loc.title().toLowerCase().indexOf(self.search.toLowerCase()) >= 0) {
                     loc.marker.setVisible(true);
                     return true;
                 } else {
                     loc.marker.setVisible(false);
+                     self.largeInfowindow.close();  //closing an info window when marker is filtered
                     return false;
                 }
             });
         }
     }, this);
 
-}
+};
 
 // Create a new blank array for all the listing markers.
-var markers = [];
 var initMap = function() {
     // Constructor creates a new map - only center and zoom are required.
     map = new google.maps.Map(document.getElementById('map'), {
         center: { lat: 40.7413549, lng: -73.9980244 },
         zoom: 13
     });
-    ko.applyBindings(viewModel);
+    ko.applyBindings(ViewModel);
 
 };
